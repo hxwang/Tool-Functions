533b1bd8d8a0c1e033e72a1d3b8933bec9963a72
Huangxin <maggiewangsjtu@gmail.com>
2014-10-03 00:21:16 -0400

    add c# tool function

--- CSharpToolFunc/README.md
+++ CSharpToolFunc/README.md
@@ -2,3 +2,4 @@ CSharpToolFunctions
 
 ### File
 - **Timer.cs**: high precision timer, in nanoseconds
+- **RunSimulation.cs**: get class by input name, this enables state in the configuration file which simulation to run
---
+++ CSharpToolFunc/RunSimulation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shuffling.Util;
+using Shuffling.Model;
+using System.Reflection;
+
+namespace Shuffling.Simulations
+{
+    public class RunSimulation
+    {
+        public static ISimulator GetSimulator(string name)
+        {
+            // get the type of the interface
+            var type = typeof(ISimulator);
+            // query all the classes that implement IMyInterface in current assembly
+            // t.IsClass is very important, otherwise you will get more interface types 
+            // that inherited IMyInterface, include IMyInterface itself
+            var q = from t in Assembly.GetExecutingAssembly().GetTypes()
+                    where t.IsClass && type.IsAssignableFrom(t)
+                    select t;
+            // convert the query result to list (List<type>)
+            var classes = q.ToList();
+            // find the one we need
+            var myClass = classes.FirstOrDefault(c => c.Name.ToLower() == name.ToLower());
+            // create an instance from a type
+            var instance = (ISimulator)Activator.CreateInstance(myClass);
+            // return the created instance       
+            return instance;
+        }
+        
+
+    }
+}
